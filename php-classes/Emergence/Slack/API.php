419fbf198ca617baad9a6de4ca6fd825b8827d90
Christopher Alfano <calfano@codeforamerica.org>
2017-08-05 22:35:16 -0400

    Add API wrapper

--- php-classes/Emergence/Slack/API.php
+++ php-classes/Emergence/Slack/API.php
@@ -8,4 +8,85 @@ class API
     public static $clientSecret;
     public static $verificationToken;
     public static $accessToken;
+    public static $baseUrl = 'https://slack.com/api';
+
+    public static function getAccessToken()
+    {
+        return static::$accessToken;
+    }
+
+    public static function request($method, array $options = [])
+    {
+        // init get params
+        if (empty($options['get'])) {
+            $options['get'] = [];
+        }
+
+        // init post params
+        if (empty($options['post'])) {
+            $options['post'] = [];
+        }
+
+        // init headers
+        if (empty($options['headers'])) {
+            $options['headers'] = [];
+        }
+
+        if (empty($options['skipAuth'])) {
+            $options['get']['token'] = empty($options['accessToken']) ? static::getAccessToken() : $options['accessToken'];
+        }
+
+        $options['headers'][] = 'User-Agent: emergence';
+
+        // init url
+        $url = static::$baseUrl . '/' . $method;
+
+        if (!empty($options['get'])) {
+            $url .= '?' . http_build_query(array_map(function($value) {
+                if (is_bool($value)) {
+                    return $value ? 'true' : 'false';
+                }
+
+                return $value;
+            }, $options['get']));
+        }
+
+        // configure curl
+        $ch = curl_init($url);
+
+        // configure output
+        if (!empty($options['outputPath'])) {
+            $fp = fopen($options['outputPath'], 'w');
+            curl_setopt($ch, CURLOPT_FILE, $fp);
+        } else {
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        }
+
+        // configure method and body
+        if (!empty($options['post'])) {
+            if (empty($options['method']) || $options['method'] == 'POST') {
+                curl_setopt($ch, CURLOPT_POST, true);
+            } else {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $options['method']);
+            }
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options['post']));
+            $options['headers'][] = 'Content-Type: application/json';
+        }
+
+        // configure headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $options['headers']);
+
+        // execute request
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        if (isset($fp)) {
+            fclose($fp);
+        } elseif (!isset($options['decodeJson']) || $options['decodeJson']) {
+            $result = json_decode($result, true);
+        }
+
+        return $result;
+    }
 }
