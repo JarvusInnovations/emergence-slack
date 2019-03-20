9cd2246b1d566198975434d9f6939ff0f8dde98d
Chris Alfano <chris@jarv.us>
2019-03-20 15:20:54 -0400

    feat: add support for sending invitations
    Uses an undocumented legacy API: https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md

--- php-classes/Emergence/Slack/Connector.php
+++ php-classes/Emergence/Slack/Connector.php
@@ -23,6 +23,8 @@ class Connector extends AbstractConnector implements IIdentityConsumer
 
     public static $teamHost;
     public static $defaultChannel = 'general';
+    public static $inviteAccountLevel = false;
+    public static $legacyToken; // obtain from https://api.slack.com/custom-integrations/legacy-tokens
 
     public static $title = 'Slack';
     public static $connectorId = 'slack';
@@ -37,6 +39,8 @@ class Connector extends AbstractConnector implements IIdentityConsumer
                 return static::handleAuthRequest();
             case 'auth-return':
                 return static::handleAuthReturnRequest();
+            case 'invite':
+                return static::handleInviteRequest();
             default:
                 return static::handleIdentityConsumerRequest($action);
         }
@@ -94,6 +98,53 @@ class Connector extends AbstractConnector implements IIdentityConsumer
     public static function handleAuthReturnRequest()
     {
         throw new \Exception('not implemented');
+    }
+
+    public static function handleInviteRequest()
+    {
+        global $Session;
+
+        if (!static::$inviteAccountLevel || !$Session) {
+            return static::throwUnauthorizedError();
+        }
+
+        $Session->requireAccountLevel(static::$inviteAccountLevel);
+
+        if (!static::$legacyToken) {
+            return static::throwError('Emergence\\Slack\\Connector::$legacyToken must be configured to send invites');
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://'.static::$teamHost.'/api/users.admin.invite');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            'token' => static::$legacyToken,
+            'email' => $Session->Person->Email,
+            'first_name' => $Session->Person->FirstName,
+            'last_name' => $Session->Person->LastName,
+            'resend' => 'true'
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+        // execute request
+        $response = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+
+        // process response
+        if (!$response['ok']) {
+            $error = 'Failed to send slack invitation';
+
+            if ($response['error']) {
+                $error .= ': '.$response['error'];
+            }
+
+            return static::throwError($error);
+        }
+
+        return static::respond('message', [
+            'message' => 'Invitation sent'
+        ]);
     }
 
     /**
